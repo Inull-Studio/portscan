f036bccbc6f15f3ec5ca0ff81cc1841c15b1ed9d
basket_ball <53385583+basket-ball@users.noreply.github.com>
2024-01-11 16:59:53 +0800

    fix build

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
-go env CGO_ENABLED=0
-go env GOOS=windows
-go env GOARCH=amd64
-go build -o releases/portscan.exe
-go env CGO_ENABLED=0
-go env GOOS=linux
-go env GOARCH=amd64
-go build -ldflags="-s -d" -o releases/portscan
+go env -w CGO_ENABLED=0
+go env -w GOOS=windows
+go env -w GOARCH=amd64
+go build -ldflags "-s" -o releases/portscan.exe
+go env -w CGO_ENABLED=0
+go env -w GOOS=linux
+go env -w GOARCH=amd64
+go build -ldflags "-s -d" -o releases/portscan
