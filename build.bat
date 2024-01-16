ffd30d79d0d50198e2e38fc7baf766c967d96aa2
basket_ball <53385583+basket-ball@users.noreply.github.com>
2024-01-16 11:28:22 +0800

    build comments

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+rem 判断是直接运行还是cmd /c调用
 if "%~1"=="" (
     goto :INIT
 ) else (
@@ -7,6 +8,7 @@ if "%~1"=="" (
 :INIT
 echo init script
 cmd /c "%~f0 1234"
+rem 在新的cmd运行自身防止污染本地环境变量，加个参数好判断
 goto :EOF
 :BUILD
 cd /d "%~dp0"
